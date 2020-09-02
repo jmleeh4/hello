4d25844c988345683dfd568d25210e541957b497
jmleeh4 <jmleeh4@naver.com>
2020-09-02 18:14:38 +0900

    qq

--- 05_HelloGit/src/com/kh/git/feature1/FeatureB.java
+++ 05_HelloGit/src/com/kh/git/feature1/FeatureB.java
@@ -1,12 +1,12 @@
 package com.kh.git.feature1;
 
 public class FeatureB {
-<<<<<<< HEAD
+
 	public void master() {
-=======
+		
+	}
+
 	public void feat() {
-		
->>>>>>> refs/heads/feature1
-		
+				
 	}
 }
